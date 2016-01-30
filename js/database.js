da9a8a33fe5f3c2805c163db279703e5cbf5750c
Diksha <Diksha@Dikshas-MacBook-Air.local>
2016-01-30 03:20:09 -0500

    Facebook login

---
+++ js/database.js
@@ -0,0 +1,126 @@
+function searchBook(term, callback){
+  var bookRef = new Firebase(FIRE_BASE_URL+BOOKS_TABLE);
+  bookRef.orderByChild("status").on("value", function(snapshot) {
+
+     var searchResult = [];
+     snapshot.forEach(function(childSnapshot) {
+        var temp = JSON.stringify(childSnapshot.val());
+        if(term){
+          var n = temp.search(term);
+          if(n>-1){
+            searchResult.push(childSnapshot.val());
+          }
+        }
+        else{
+         searchResult.push(childSnapshot.val());
+        }
+      });
+
+      callback(searchResult);
+  });
+}
+
+function saveBook(book, callback){
+  var bookRef = new Firebase(FIRE_BASE_URL+BOOKS_TABLE);
+  var book_data = {};
+  book_data[book.isbn] = book;
+  bookRef.update(book_data, callback);
+}
+
+function updateBook(book, callback){
+  var bookRef = new Firebase(FIRE_BASE_URL+BOOKS_TABLE+book.isbn);
+  bookRef.update(book, callback);
+}
+
+function getMyBooks(uid, callback){
+  var return_data = [];
+  var bookRef = new Firebase(FIRE_BASE_URL+BOOKS_TABLE);
+  bookRef.orderByChild("uid").equalTo(uid).on("value", function(snapshot) {
+    snapshot.forEach(function(data){
+      return_data.push(data.val());
+    });
+    callback(return_data);
+  });
+}
+
+function getBorrowedBooks(uid, callback){
+  var return_data = [];
+  var bookRef = new Firebase(FIRE_BASE_URL+BOOKS_TABLE);
+  bookRef.orderByChild("borrow_uid").equalTo(uid).on("value", function(snapshot) {
+    snapshot.forEach(function(data){
+      return_data.push(data.val());
+    });
+    callback(return_data);
+  });
+}
+
+function getUser(uid, callback){
+var user_data = [];
+var userRef = new Firebase(FIRE_BASE_URL+USERS_TABLE+uid);
+
+userRef.once('value', function(data) {
+	//console.log(data.val());
+  user_data.push(data.val());
+	callback(user_data);
+	});
+}
+
+
+getUser('facebook:1037502162960482', function(data){
+    data.forEach(function(innerData){
+        //console.log(innerData.fname);
+    });
+});
+
+function borrow_book(uid, isbn, callback){
+  var bookRef = new Firebase(FIRE_BASE_URL+BOOKS_TABLE);
+  var singleBookRef = new Firebase(FIRE_BASE_URL+BOOKS_TABLE+"/"+isbn);
+  var userRef = new Firebase(FIRE_BASE_URL+USERS_TABLE);
+  var borrowedRef = new Firebase(FIRE_BASE_URL+USERS_TABLE+"/"+uid+'/nu_borrowed');
+  var bookOwner = null;
+
+  bookRef.child(isbn).update({
+    "status":0,
+    "borrow_uid": uid
+  });
+
+  borrowedRef.transaction(function(current_value){
+    return (parseInt(current_value) || 0) +1;
+  });
+
+  singleBookRef.once("value", function(snapshot){
+    bookOwner = snapshot.val().uid;
+    console.log(bookOwner);
+    var ownerRef = new Firebase(FIRE_BASE_URL+USERS_TABLE+"/"+bookOwner+'/nu_lent');
+    ownerRef.transaction(function(current_value){
+      return (parseInt(current_value) || 0) +1;
+    });
+    ownerRef = new Firebase(FIRE_BASE_URL+USERS_TABLE+"/"+bookOwner+'/price_lent');
+    ownerRef.transaction(function(current_value){
+      return (parseInt(current_value) || 0) + parseInt(snapshot.val().price);
+    });
+
+  borrowedRef = new Firebase(FIRE_BASE_URL+USERS_TABLE+"/"+uid+'/price_borrowed');
+    borrowedRef.transaction(function(current_value){
+      return (parseInt(current_value) || 0) + parseInt(snapshot.val().price);
+    });
+
+  });
+
+}
+/*
+function donateToLibrary(amount){
+  var data = {"medium": "balance", "payee_id": LIBRARY_ACCOUNT_ID, "amount" : amount };
+  console.log(JSON.stringify(data));
+  console.log(JSON.stringify(ACCOUNT_URL+DEBIT_ACCOUNT_ID+TRANSFER_URL+CAPITAL_ONE_QUERY_PARAM));
+    alert(JSON.stringify(ACCOUNT_URL+DEBIT_ACCOUNT_ID+TRANSFER_URL+CAPITAL_ONE_QUERY_PARAM));
+    $.ajax({
+      url: ACCOUNT_URL+DEBIT_ACCOUNT_ID+TRANSFER_URL+CAPITAL_ONE_QUERY_PARAM,
+      type: "POST",
+      data: data,
+    //http://api.reimaginebanking.com/accounts/56241a14de4bf40b17112a77/transfers?key=2ec3d395b0e81344514ca1ecbae6edcb
+      success: function(results){
+         alert(results);
+      }
+    });
+}*/
---
+++ js/login.js
@@ -0,0 +1,66 @@
+ref.onAuth(function(authData) {
+  if (authData && isNewUser) {
+    // save the user's profile into the database so we can list users,
+    // use them in Security and Firebase Rules, and show profiles
+    ref.child("users").child(authData.facebook.id).set({
+      fname: getFName(authData),
+      lname: getLName(authData),
+      id: getId(authData)
+    });
+  }
+});
+
+function getId(authData){
+	return authData.facebook.id;
+}
+
+function getFName(authData){
+	return authData.facebook.cachedUserProfile.first_name;
+}
+
+function getLName(authData){
+	return authData.facebook.cachedUserProfile.last_name;
+}
+
+function login(){
+
+	ref.authWithOAuthPopup("facebook", function(error, authData) {
+  		if (error) {
+   			console.log("Login Failed!", error);
+  		} else {
+  			console.log("Authenticated successfully with payload:", authData.facebook.cachedUserProfile.first_name);
+  			checkSession();
+  		}
+	}, {
+  		remember: "sessionOnly"
+	});
+}
+
+function logout(){
+	ref.unauth();
+	window.location.href = "index.html";
+}
+
+function checkSession(){
+	authData = ref.getAuth();
+	console.log(authData);
+	if(authData){
+		window.location.href = "pages/index.html";
+	}
+}
+
+function checkSessionLogin(){
+	authData = ref.getAuth();
+	console.log(authData);
+	if(authData == null){
+		window.location.href = "index.html";
+	}else{
+		window.full_name = getFName(authData) + " "+ getLName(authData);
+		$(".username").html("&nbsp;&nbsp;" + window.full_name);
+	}
+}
+
+$("#logout_anchor").click(function(){
+    logout();
+});
+
